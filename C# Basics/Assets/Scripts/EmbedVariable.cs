caafc40004be60081b239b46f103155ff653bd6c
siryu-saito <saito.s.ryu@gmail.com>
2022-02-05 21:52:31 +0900

    クラス定義

--- C# Basics/Assets/Scripts/EmbedVariable.cs
+++ C# Basics/Assets/Scripts/EmbedVariable.cs
@@ -2,6 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+class Animal
+{
+    public int age = 2;
+    public string kind = "犬";
+
+    public void Bite()
+    {
+        Debug.Log($"{age}才の{kind}が噛み付く");
+    }
+
+}
+
 public class EmbedVariable : MonoBehaviour
 {
 
@@ -10,13 +22,10 @@ public class EmbedVariable : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int[] numbers = { 1, 2, 3, 4, 5, 6 };
+        Animal dog = new Animal();
 
-        foreach(int number in numbers)
-        {
-            Debug.Log(number);
-        }
-
+        Debug.Log(dog.kind);
+        dog.Bite();
     }
 
     // Update is called once per frame
